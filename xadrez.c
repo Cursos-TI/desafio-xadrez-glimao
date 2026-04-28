850e55f29502cb4161748605b23b4232c3a8a7d1
glimao <glima.smor@gmail.com>
2026-04-28 04:07:20 +0000

    Adicionei a recursividade para melhorar a movimentação da rainha, da torre e do bispo Utilizei loops complexos para o cavalo, com o uso de Continue e Break

--- xadrez.c
+++ xadrez.c
@@ -1,53 +1,47 @@
 #include <stdio.h>
 
+void movimento(int casas, char *posicao){
+    if (casas > 0){
+        printf("%s\n", posicao);
+        movimento(casas - 1, posicao);
+    }
+}
+
+void bispo(int casas){
+    for (int i = 1; i <= casas; i++){      
+        for (int j = 1; j <= 1; j++){      
+            printf("Cima\n");
+            printf("Direita\n");
+        }
+    }
+}
+
 int main(){
-    int torre, bispo, rainha;
-    int cavalo;
 
     // Torre
-    printf("\n=== MOVIMENTANDO A TORRE===\n");
-    for (torre = 1; torre <= 5; torre++){
-        printf("Direita %dx\n", torre);
-    }
+    printf("\n=== MOVIMENTANDO A TORRE ===\n");
+    movimento(5, "Direita");
 
     // Bispo
-    bispo = 1;
-    printf("\n=== MOVIMENTANDO O BISPO===\n");
-    while (bispo <= 5)
-    {
-        printf("Cima, direita %dx\n", bispo);
-        bispo++;
-    }
+    printf("\n=== MOVIMENTANDO O BISPO ===\n");
+    bispo(5);
 
     // Rainha
-    rainha = 1;
-    printf("\n=== MOVIMENTANDO A RAINHA===\n");
-    do
-    {
-        printf("Esquerda %dx\n", rainha);
-        rainha++;
-    } while (rainha <= 8);
+    printf("\n=== MOVIMENTANDO A RAINHA ===\n");
+    movimento(8, "Esquerda");
 
     // Cavalo
-    int passos_baixo = 2;
-    int passos_esquerda = 1;
-    int passo_atual = 0;
 
     printf("\n=== MOVIMENTANDO O CAVALO===\n");
-    do
-    {
-        for (cavalo = 1; cavalo <= passos_baixo; cavalo++)
-        {
-            printf("Baixo\n");
-        }
-        for (int e = 1; e <= passos_esquerda; e++)
-        {
-            printf("Esquerda\n");
-        }
-        passo_atual++;
-    } while (passo_atual < 1);
-        
-        
+    
+    for (int i = 1; i <= 3; i++){
+    if (i <= 2){
+        printf("Cima\n");
+        continue;
+    }
+    printf("Direita\n");
+    break;
+    }
     return 0;
 
 }
